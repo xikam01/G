d6b5da7acd7624e28f184d7c8c66d87dc61a0737
xikam01 <cabdiraxmaanxikam190@mail.com>
2026-06-22 16:31:36 -0700

    done

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// document.getElementById("callme").textContent=("call back fuction")
+document.getElementById("callme").textContent=("call back fuction")
 
 
-// let age =40;
-// let price =10.99;
-// let gpa=2.1;
+let age =40;
+let price =10.99;
+let gpa=2.1;
 
 
-// console.log(`You are ${age}year old`)
-// console.log(`THE PRICE IS  ${price}`)
-// console.log(`Your gpa is  ${gpa}`)
+console.log(`You are ${age}year old`)
+console.log(`THE PRICE IS  ${price}`)
+console.log(`Your gpa is  ${gpa}`)
